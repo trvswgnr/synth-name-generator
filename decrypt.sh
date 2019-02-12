280bec286d254b9e2cbd04998a6228a5e68e6f1d
trav <trvswgnr@gmail.com>
2019-02-11 23:31:06 -0500

    update

--- decrypt.sh
+++ decrypt.sh
@@ -1,5 +1,1 @@
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then exit 0; fi # dont run if is a pull request
 openssl aes-256-cbc -K $encrypted_615321f5512c_key -iv $encrypted_615321f5512c_iv -in deploy_rsa.enc -out /tmp/deploy_rsa -d
-eval "$(ssh-agent -s)"
-chmod 600 /tmp/deploy_rsa
-ssh-add /tmp/deploy_rsa
